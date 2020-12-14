1c2e79819f1f7b9bfe50aed21ef4498919c29d5d
Defeat3d <koen.deubel@hva.nl>
2020-12-14 22:20:58 +0100

    Change docker image

--- docker_run.bash
+++ docker_run.bash
@@ -16,8 +16,8 @@ usage() {
 
 [ $# -eq 0 ] && usage
 
-alerter="email"
-default_image="ericjmarti/inventory-hunter:latest"
+alerter="discord"
+default_image="defeat3d/inv-hunter:latest"
 image=$default_image
 
 while getopts a:c:d:e:i:q:r:w: arg

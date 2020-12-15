f618d2fc7fae98682fe35578a254f39b01b360ac
Defeat3d <koen.deubel@hva.nl>
2020-12-15 14:32:55 +0100

    Fuck

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -2,12 +2,15 @@ import scraper.adorama
 import scraper.amazon
 import scraper.bestbuy
 import scraper.bhphotovideo
-import scraper.microcenter
-import scraper.newegg
-import scraper.walmart
 import scraper.bol
 import scraper.coolblue
+import scraper.gamemania
+import scraper.intertoys
 import scraper.mediamarkt
+import scraper.microcenter
+import scraper.nedgame
+import scraper.newegg
+import scraper.walmart
 
 from scraper.common import ScraperFactory
 

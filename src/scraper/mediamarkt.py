8c099d87f8ad87526ce8ccd939c112a8551ad9b2
Defeat3d <koen.deubel@hva.nl>
2020-12-16 15:28:44 +0100

    Use meta analysis for mediamarkt

--- src/scraper/mediamarkt.py
+++ src/scraper/mediamarkt.py
@@ -8,7 +8,6 @@ class MediamarktScrapeResult(ScrapeResult):
 
         # get name of product
         tag = self.soup.find('meta', property='og:title')
-        self.logger.warning(f'title: {tag}')
         if tag:
             alert_content += tag['content'] + '\n'
         else:
@@ -16,14 +15,12 @@ class MediamarktScrapeResult(ScrapeResult):
 
         # get listed price
         tag = self.soup.find('meta', property='product:price:amount')
-        self.logger.warning(f'price: {tag}')
         price_str = self.set_price(tag['content'])
         if price_str:
             alert_subject = f'In Stock for {price_str}'
 
         # check for add to cart button
         tag = self.soup.find('meta', property='og:availability')
-        self.logger.warning(f'availability: {tag}')
         if tag and 'uitverkocht' not in tag['content']:
             self.alert_subject = alert_subject
             self.alert_content = f'{alert_content.strip()}\n{self.url}'
